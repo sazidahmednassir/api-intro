55a34f229a6cd0ae6e2bff87c71c69702cc278bd
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-21 21:50:18 -0800

    third  commit

--- js/post.js
+++ js/post.js
@@ -11,6 +11,7 @@ function displayPosts(posts){
     // console.log(posts)
     for(const post of posts){
         const div= document.createElement('div');
+        div.classList.add('post')
         div.innerHTML= ` <h3>${post.title}</h3> 
         <p> ${post.body}</p>`
 
