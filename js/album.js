b5218e52dd55fd612a0b3bd53c63c69f37de605f
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-21 22:55:28 -0800

    sixtht  commit

--- js/album.js
+++ js/album.js
@@ -3,3 +3,16 @@ function loadAlbums() {
         .then(res => res.json())
         .then(data => displayAlbums(data));
 }
+
+loadAlbums()
+
+function displayAlbums(albums) {
+    console.log(albums)
+    const albumContainer = document.getElementById('albums');
+    for (const album of albums) {
+        const p = document.createElement('p');
+        p.style.textAlign = 'center';
+        p.innerText = album.title;
+        albumContainer.appendChild(p);
+    }
+}
